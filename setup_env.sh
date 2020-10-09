49dd1722bdab8dcc31c81bfdacbbb9fad33c391f
Dario Gonzalez Bano <Dario.GonzalezBano@thomsonreuters.com>
2020-10-09 13:46:43 -0300

    add script to generate enviroment file

---
+++ setup_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo NODE_ENV=$NODE_ENV >> "src/environments/${1}.env"
+echo MONGO_USER=$MONGO_USER >> "src/environments/${1}.env"
+echo MONGO_PASSWORD=$MONGO_PASSWORD >> "src/environments/${1}.env"
+echo MONGO_PATH=$MONGO_PATH >> "src/environments/${1}.env"
+echo MONGO_DATABASE=$MONGO_DATABASE >> "src/environments/${1}.env"
+echo JWT_SECRET=$JWT_SECRET >> "src/environments/${1}.env"
+echo PORT=$PORT >> "src/environments/${1}.env"
