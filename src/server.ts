6d5fb80b978f92f9a335db3ce9e5465ccbdb3c78
Dario Gonzalez Bano <Dario.GonzalezBano@thomsonreuters.com>
2020-10-09 19:27:58 -0300

    add untracked file in ci yml

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import 'dotenv/config';
-import colors from 'colors';
+import 'colors';
 import App from './app';
 import validateEnv from './utils/validateEnv';
 import { IndexController, AuthController, UserController } from './controllers';
-
-colors.enable();
 
 validateEnv();
 
