e3fb01cdd49a3818fc864612a2fbf4a186a5c170
Dario Gonzalez Bano <Dario.GonzalezBano@thomsonreuters.com>
2020-10-17 18:31:26 -0300

    fix lint problems

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -3,25 +3,25 @@ import { App } from '@/config/index';
 import { IndexController } from '@/controllers';
 
 describe('Testing Index', () => {
-  
   afterAll(async () => {
-    await new Promise(resolve => setTimeout(() => resolve(), 500));
+    await new Promise((resolve) => setTimeout(() => resolve(), 500));
   });
 
   let indexController: IndexController;
   let app: App;
-  
+
   beforeAll(async () => {
     indexController = new IndexController();
     app = new App([indexController]);
   });
 
-  describe('[GET] /', () => { 
+  describe('[GET] /', () => {
     it('response statusCode 200', async () => {
-      const { status, body } = await request(app.getServer()).get(`${indexController.path}`);
+      const { status, body } = await request(app.getServer()).get(
+        `${indexController.path}`
+      );
       expect(status).toBe(200);
       expect(body.message).toBeDefined();
     });
   });
-
 });
